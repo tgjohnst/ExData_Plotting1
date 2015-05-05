e44945b7f7464c4b489f1b27aa0ac29e5dd999b6
Timothy Johnstone <timothy.g.johnstone@gmail.com>
2015-05-04 23:45:20 -0400

    all scripts now read in data and pre-process it

--- scripts/plot1.R
+++ scripts/plot1.R
@@ -3,9 +3,25 @@
 ## Course 4: Exploratory Data Analysis (exdata-014)
 ## Timothy Johnstone, May 2015
 
+# Set working directory
+# TO RUN THIS SCRIPT please modify the path to reflect your system's file structure
+setwd("Z:/Lotus/Dropbox/Coursera/4_Exploratory_Data_Analysis/Projects/ExData_Plotting1")
+
 ##########
 
-# Load in test dataset file
-testx <- read.table("test/X_test.txt")
+# Load in dataset file from compressed archive
+exdata <- read.table(unz('data/exdata_data_household_power_consumption.zip','household_power_consumption.txt'), 
+                    header=T,
+                    sep=";", 
+                    na.strings="?"
+                    )
+
+# Subset just to the dates we care about
+exdata <- exdata[exdata$Date == "2/2/2007" | exdata$Date == "1/2/2007" ,]
+
+# Pre-process dataset columns
+exdata$Date = as.Date(exdata$Date,format = '%d/%m/%Y')
+exdata$Time = strptime(exdata$Time, format="%H:%M:%S" )
+
 
 ##########
--- scripts/plot2.R
+++ scripts/plot2.R
@@ -2,3 +2,26 @@
 ## Analysis script for the first project of Coursera Data Science Series, 
 ## Course 4: Exploratory Data Analysis (exdata-014)
 ## Timothy Johnstone, May 2015
+
+# Set working directory
+# TO RUN THIS SCRIPT please modify the path to reflect your system's file structure
+setwd("Z:/Lotus/Dropbox/Coursera/4_Exploratory_Data_Analysis/Projects/ExData_Plotting1")
+
+##########
+
+# Load in dataset file from compressed archive
+exdata <- read.table(unz('data/exdata_data_household_power_consumption.zip','household_power_consumption.txt'), 
+                     header=T,
+                     sep=";", 
+                     na.strings="?"
+)
+
+# Subset just to the dates we care about
+exdata <- exdata[exdata$Date == "2/2/2007" | exdata$Date == "1/2/2007" ,]
+
+# Pre-process dataset columns
+exdata$Date = as.Date(exdata$Date,format = '%d/%m/%Y')
+exdata$Time = strptime(exdata$Time, format="%H:%M:%S" )
+
+
+##########
